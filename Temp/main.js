bbd6bf26988ffa9566db61b92187906c403a1e58
Piyush Maurya <PiyushMaurya.cs@gmail.com>
2025-07-18 00:43:52 +0530

    function

--- Temp/main.js
+++ Temp/main.js
@@ -1,6 +1,5 @@
-let myDate = new Date();
-console.log(myDate);
-console.log(myDate.toString());
-console.log(myDate.toISOString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleDateString());
+function loginUserMessage(username){
+  return `${username} just logged in`;
+}
+let result = loginUserMessage("Piyush");
+console.log(result);
